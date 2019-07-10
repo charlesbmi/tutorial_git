9bfc9d23b1e6c2737bf4405b34739a658fc735f8
Charles Guan <charlesguan94@gmail.com>
2019-07-10 11:04:44 -0700

    Add buggy MATLAB script for cherry-pick demo

---
+++ buggy_rand.m
@@ -0,0 +1,9 @@
+%BUGGY_RAND Uniformly distributed pseudorandom number.
+% Broken function, using for branching tutorial
+function r = buggy_rand()
+
+% Hard-code random number for testing. TODO: don't hard-code in future
+r = 42;
+% r = rand(1);
+
+end
---
+++ print_rand.m
@@ -0,0 +1,5 @@
+% Print a random number using self-written rand function
+
+r = buggy_rand();
+disp('Random number:')
+disp(r);
